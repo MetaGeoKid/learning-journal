b98294fefd412aa65a93f4a94e4dc5b8b74bff5a
MetaGeoKid <73967741+MetaGeoKid@users.noreply.github.com>
2021-03-24 12:17:29 -0500

    Update forms.py

--- forms.py
+++ forms.py
@@ -1,12 +1,12 @@
 from flask_wtf import Form
-from wtforms import StringField, PasswordField, TextAreaField, DateField, FloatField
+from wtforms import TextAreaField
 from wtforms.validators import (DataRequired, Regexp, ValidationError, Email,
                                Length, EqualTo)
 
 
 class PostForm(Form):
     title = TextAreaField(validators=[DataRequired()])
-    date = DateField(validators=[DataRequired()])
+    date = TextAreaField(validators=[DataRequired()])
     time_spent = FloatField("How many hours?", validators=[DataRequired()])
     content = TextAreaField("What did you learn?", validators=[DataRequired()])
     resources = TextAreaField(validators=[DataRequired()])
